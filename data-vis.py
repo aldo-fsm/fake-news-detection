2fa9917ed7cb8bb40f32a0108731c709a2c48703
Aldo Monteiro <aldo.monteiro@pitang.com>
2020-12-13 18:58:43 -0300

    Label and category distribution visualization

--- data-vis.py
+++ data-vis.py
@@ -1,3 +1,4 @@
+import plotly_express as px
 import streamlit as st
 
 from datasets import fake_br_corpus
@@ -5,3 +6,9 @@ from datasets import fake_br_corpus
 train = fake_br_corpus.loadTrain()
 
 train
+
+st.write(
+    px.pie(train, names='label'),
+    px.pie(train, names='category'),
+    px.histogram(train, x='category', color='label', barmode='group'),
+)
