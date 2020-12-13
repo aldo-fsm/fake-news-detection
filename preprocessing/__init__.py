11163e2a67a8d9b3910f43ed8ceae8bc185c4ea9
Aldo Monteiro <aldo.monteiro@pitang.com>
2020-12-13 19:00:19 -0300

    preprocessing utility functions

---
+++ preprocessing/__init__.py
@@ -0,0 +1,21 @@
+import re
+
+# import spacy
+# nlp = spacy.load('pt')
+
+def lemmatize(text):
+    global tokens
+    tokens = nlp(text)
+    lemmas = [token.lemma_ for token in tokens]
+    return ' '.join(lemmas)
+
+def cleanText(text):
+    text = re.sub(r'[0-9"\(\)\,\:]+', ' ', text)
+    text = re.sub(r'[\n ]+', ' ', text)
+    text = text.lower()
+    return text
+
+def preprocess(text):
+#     text = lemmatize(text)
+    text = cleanText(text)
+    return text
