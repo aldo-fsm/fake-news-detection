80e4d20e9adfc671085a99e2ee1701d5f589a881
Aldo Monteiro <aldo.monteiro@pitang.com>
2021-01-08 20:30:32 -0300

    lime and shap

--- data-vis.py
+++ data-vis.py
@@ -12,3 +12,28 @@ st.write(
     px.pie(train, names='category'),
     px.histogram(train, x='category', color='label', barmode='group'),
 )
+
+# from preprocessing import preprocess
+# from sklearn.feature_extraction.text import TfidfVectorizer
+
+# tfidf = TfidfVectorizer(
+#     preprocessor=preprocess,
+#     ngram_range=(1,3),
+#     max_features=1000
+# )
+
+# st.cache()
+# def extractFeatures(texts):
+#     return tfidf.fit_transform(texts)
+
+# X = extractFeatures(train.text)
+
+# from sklearn.decomposition import PCA
+
+# pca = PCA(2)
+# points = pca.fit_transform(X.toarray())
+# # train[['pcax','y','z']] = points
+
+# st.write(
+#     px.scatter(x=points.T[0], y=points.T[1], color=train.label)
+# )
